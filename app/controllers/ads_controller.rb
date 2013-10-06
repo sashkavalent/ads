ead1a8e2c7a33e84ad44879e14a1be2322fd1ab9
sashkavalent <sashkavalent@gmail.com>
2013-10-06 14:12:16 +0300

    Cancan

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -2,12 +2,16 @@ class AdsController < ApplicationController
 	def create
 		@ad = current_user.ads.build(params[:ad])
 		if @ad.save 
-			flash[:success] = "Ad is created"
+			flash[:notice] = "Ad is created"
 			redirect_to root_path
 		else
 			render root_path
 		end	
 	end
 	def destroy
+		@ad = Ad.find_by_id(params[:id])
+		@ad.destroy
+		flash[:notice] = "Ad is deleted"
+		redirect_to root_path
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :role
   # attr_accessible :title, :body
   has_many :ads, dependent: :destroy
+  ROLES = %w[guest user admin]
+  def role?(base_role)
+  	ROLES.index(base_role.to_s) <= ROLES.index(role)
+  end
 end
