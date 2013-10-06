d2e26baa8ee5bc1fec21462f11a135f54c883908
sashkavalent <sashkavalent@gmail.com>
2013-10-06 20:26:18 +0300

    On heroku

--- config/application.rb
+++ config/application.rb
@@ -66,6 +66,6 @@ module Ads
     config.assets.version = '1.0'
 
     ###### From devise install console
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
   end
 end
