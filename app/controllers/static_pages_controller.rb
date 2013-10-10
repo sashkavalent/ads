0987bb076ad1996eaac45af1c2f5ad608a279922
sashkavalent <sashkavalent@gmail.com>
2013-10-11 00:54:18 +0300

    with profile and ad editing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,7 @@
 class StaticPagesController < ApplicationController
 	def home
-			@ads = Ad.paginate(:page => params[:page], :per_page => 20)
-			@ad = current_user.ads.build if user_signed_in?
-	end	
+
+	end
 	def help
 	end
 	def about
