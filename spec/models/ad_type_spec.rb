330c3d7c498a3d92e8e9af25bb314d5ad612cf86
sashkavalent <sashkavalent@gmail.com>
2013-10-24 01:43:55 +0300

    fix bugs in tests and add new ones

--- spec/models/ad_type_spec.rb
+++ spec/models/ad_type_spec.rb
@@ -7,18 +7,13 @@ describe AdType do
   subject { @ad_type }
   it { should respond_to(:name) }
 
-  describe "when user_id is not present" do
-    before { @ad.user_id = nil }
-    it { should_not be_valid }
-  end
-
   describe "with blank content" do
-    before { @ad.content = " " }
+    before { @ad_type.name = " " }
     it { should_not be_valid }
   end
 
   describe "with content that is too long" do
-    before { @ad.content = "a" * 201 }
+    before { @ad_type.name = "a" * 201 }
     it { should_not be_valid }
   end
 end
