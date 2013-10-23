120259a97901241ffef347fe15de8b6ab5fdb7fe
sashkavalent <sashkavalent@gmail.com>
2013-10-24 02:10:03 +0300

    Fixes #5

--- Gemfile
+++ Gemfile
@@ -2,21 +2,21 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
 gem 'faker', '1.0.1'
-gem "state_machine", "~> 1.2.0"
+gem 'state_machine', '~> 1.2.0'
 gem 'will_paginate', '~> 3.0.5'
 gem 'bootstrap-will_paginate', '0.0.6'
 gem 'pg'
-gem "cancan"
+gem 'cancan'
 gem 'devise'
 gem 'bootstrap-sass', '2.1'
-gem "nested_form"
-gem "paperclip", "~> 3.0"
-gem "whenever"
-gem "paperclip-dropbox"
+gem 'nested_form'
+gem 'paperclip', '~> 3.0'
+gem 'whenever'
+gem 'paperclip-dropbox'
 gem 'hermitage'
 gem 'childprocess', '0.3.6'
 group :development, :test do
-	gem "pry", "0.9.12.2"
+	gem 'pry', '0.9.12.2'
   gem 'rspec-rails', '2.11.0'
   gem 'guard-rspec', '1.2.1'
   gem 'guard-spork', '1.2.0'
--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,50 +1,50 @@
 class AdsController < ApplicationController
-	load_and_authorize_resource
+  load_and_authorize_resource
 
-	before_filter :load_ad, :only => [:destroy, :update, :show, :change_state]
+  before_filter :load_ad, :only => [:destroy, :update, :show, :change_state]
 
-	def create
-		@ad = current_user.ads.build(params[:ad])
-		if @ad.save
-			flash[:notice] = "Ad is created"
-		else
-			flash[:error] = @ad.errors.full_messages.join(". ")
-		end
-		redirect_to(:back)
-	end
+  def create
+    @ad = current_user.ads.build(params[:ad])
+    if @ad.save
+      flash[:notice] = "Ad is created"
+    else
+      flash[:error] = @ad.errors.full_messages.join(". ")
+    end
+    redirect_to(:back)
+  end
 
-	def destroy
-		@ad.destroy
-		flash[:notice] = "Ad is deleted"
-		redirect_to(:back)
-	end
+  def destroy
+    @ad.destroy
+    flash[:notice] = "Ad is deleted"
+    redirect_to(:back)
+  end
 
-	def index
-		@ads = Ad.where(state: "published").paginate(:page => params[:page], :per_page => 8)
-		render '_ads'
-	end
+  def index
+    @ads = Ad.where(state: "published").paginate(:page => params[:page], :per_page => 8)
+    render '_ads'
+  end
 
-	def update
-		@ad.attributes = params[:ad]
-		@ad.save
-		redirect_to profile_path
-	end
+  def update
+    @ad.attributes = params[:ad]
+    @ad.save
+    redirect_to profile_path
+  end
 
-	def show
-		session[:return_to] ||= request.referer
-	end
+  def show
+    session[:return_to] ||= request.referer
+  end
 
-	def change_state
-		@ad.public_send(params[:state_event])
-		@ad.save
-		flash[:notice] = "Status of ad was changed"
-		redirect_to(:back)
-	end
+  def change_state
+    @ad.public_send(params[:state_event])
+    @ad.save
+    flash[:notice] = "Status of ad was changed"
+    redirect_to(:back)
+  end
 
-	protected
+  protected
 
-		def load_ad
-			@ad = Ad.find_by_id(params[:id])
-		end
+    def load_ad
+      @ad = Ad.find_by_id(params[:id])
+    end
 
 end
