27df6b441e760fd383af196022ff1184894658f6
sashkavalent <sashkavalent@gmail.com>
2013-10-24 02:10:03 +0300

    Fixes #5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,20 @@
 module ApplicationHelper
 
-	def full_title(page_title)
-		base_title = "Ads"
-		if page_title.empty?
-			base_title
-		else
-			"#{base_title} | #{page_title}"
-		end
-	end
+  def full_title(page_title)
+    base_title = "Ads"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 
-	def flash_class(type)
-		case type
-			when :notice then "alert alert-info"
-			when :alert then "alert alert-error"
-			when :error then "alert alert-erro"
-			when :success then "alert alert-success"
-		end
-	end
+  def flash_class(type)
+    case type
+      when :notice then "alert alert-info"
+      when :alert then "alert alert-error"
+      when :error then "alert alert-erro"
+      when :success then "alert alert-success"
+    end
+  end
 end
