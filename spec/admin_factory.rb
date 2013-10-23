7cda855ac6df98fdcc06e48de892c0536885ebef
sashkavalent <sashkavalent@gmail.com>
2013-10-24 01:49:22 +0300

    FactoryGirl in tests

--- spec/admin_factory.rb
+++ spec/admin_factory.rb
@@ -1,12 +1,5 @@
 FactoryGirl.define do
 
-  factory :user do
-    email    "sasha@example.com"
-    password "12345678"
-    password_confirmation "12345678"
-    remember_me false
-  end
-
   factory :admin, class: User do
     email    "admin@example.com"
     password "12345678"
