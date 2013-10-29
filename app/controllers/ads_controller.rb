1707482321c3078a5c082c0ae501cb2f0afae612
sashkavalent <sashkavalent@gmail.com>
2013-10-29 23:12:28 +0300

    Fixes #12

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,9 +1,8 @@
 class AdsController < ApplicationController
   load_and_authorize_resource
 
-  before_filter :load_ad, :only => [:destroy, :update, :show, :change_state]
-
   def create
+
     @ad = current_user.ads.build(params[:ad])
     if @ad.save
       flash[:notice] = t(:added, scope: [:ads])
@@ -11,12 +10,15 @@ class AdsController < ApplicationController
       flash[:error] = @ad.errors.full_messages.join('. ')
     end
     redirect_to(:back)
+
   end
 
   def destroy
+
     @ad.destroy
     flash[:notice] = t(:deleted, scope: [:ads])
     redirect_to(:back)
+
   end
 
   def index
@@ -30,9 +32,11 @@ class AdsController < ApplicationController
   end
 
   def update
+
     @ad.attributes = params[:ad]
     @ad.save
     redirect_to profile_path
+
   end
 
   def show
@@ -41,16 +45,12 @@ class AdsController < ApplicationController
   end
 
   def change_state
+
     @ad.public_send(params[:state_event])
     @ad.save
     flash[:notice] = t(:status_changed, scope: [:ads])
     redirect_to(:back)
+
   end
-
-  protected
-
-    def load_ad
-      @ad = Ad.find_by_id(params[:id])
-    end
 
 end
