cca5dd03d6f283cb66a42115320c27188b4c8801
sashkavalent <sashkavalent@gmail.com>
2013-12-03 13:00:11 +0300

    html2haml

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -3,4 +3,5 @@ class Announcement < ActiveRecord::Base
   belongs_to :user
   belongs_to :ad
   validates :content, :ad_id, presence: true
+  default_scope order: 'announcements.created_at DESC'
 end
